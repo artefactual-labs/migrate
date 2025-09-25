c313d4afc32c3d9724742eadf8b2134139d63b78
Jesús García Crespo <jesus@sevein.com>
2025-09-25 18:56:20 +0200

    Add tests for cmd/list-filter

--- cmd/list-filter/main.go
+++ cmd/list-filter/main.go
@@ -2,73 +2,106 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/artefactual-labs/migrate/internal/application"
 )
 
 func main() {
-	mapFilter := map[string]struct{}{}
-	finalListSlice := []string{}
+	var (
+		args   = os.Args[1:]
+		stdin  = os.Stdin
+		stdout = os.Stdout
+		stderr = os.Stderr
+	)
 
-	var filterOutInput []string
-	f, err := os.Open("to_filter_out.txt")
-	application.PanicIfErr(err)
-	s := bufio.NewScanner(f)
-	toFilterCount := 0
-	for s.Scan() {
-		toFilterCount++
-		mapFilter[s.Text()] = struct{}{}
-		filterOutInput = append(filterOutInput, s.Text())
+	if err := exec(args, stdin, stdout, stderr); err != nil {
+		printf(stderr, "error: %v\n", err)
+		os.Exit(1)
 	}
-	_, err = application.ValidateUUIDs(filterOutInput)
+}
+
+func exec(_ []string, _ io.Reader, stdout, _ io.Writer) error {
+	filterList, err := readLines("to_filter_out.txt")
 	if err != nil {
-		application.PanicIfErr(err)
+		return fmt.Errorf("read to_filter_out.txt: %v", err)
 	}
 
-	originalCount := 0
-	{
-		var input []string
-		f, err := os.Open("original_list.txt")
-		application.PanicIfErr(err)
-		s := bufio.NewScanner(f)
-		for s.Scan() {
-			if _, ok := mapFilter[s.Text()]; !ok {
-				finalListSlice = append(finalListSlice, s.Text())
-			}
-			input = append(input, s.Text())
-			originalCount++
-		}
-		_, err = application.ValidateUUIDs(input)
-		if err != nil {
-			application.PanicIfErr(err)
-		}
+	if _, err := application.ValidateUUIDs(filterList); err != nil {
+		return fmt.Errorf("validate to_filter_out.txt: %v", err)
 	}
-	finalCount := 0
-	{
-		if err := os.Remove("final_list.txt"); err != nil {
-			application.PanicIfErr(err)
-		}
 
-		f, err := os.OpenFile("final_list.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o644)
-		application.PanicIfErr(err)
-		for _, v := range finalListSlice {
-			_, err := f.WriteString(v + "\n")
-			application.PanicIfErr(err)
+	originalList, err := readLines("original_list.txt")
+	if err != nil {
+		return fmt.Errorf("read original_list.txt: %v", err)
+	}
+	if _, err := application.ValidateUUIDs(originalList); err != nil {
+		return fmt.Errorf("validate original_list.txt: %v", err)
+	}
+
+	filterSet := make(map[string]struct{}, len(filterList))
+	for _, v := range filterList {
+		filterSet[v] = struct{}{}
+	}
+
+	finalList := make([]string, 0, len(originalList))
+	for _, v := range originalList {
+		if _, exists := filterSet[v]; exists {
+			continue
 		}
-		f.Close() //nolint:errcheck
-		{
-			f, err := os.Open("final_list.txt")
-			application.PanicIfErr(err)
-			defer f.Close() //nolint:errcheck
-			s := bufio.NewScanner(f)
-			for s.Scan() {
-				finalCount++
-			}
+		finalList = append(finalList, v)
+	}
+
+	if err := writeLines("final_list.txt", finalList); err != nil {
+		return fmt.Errorf("write final_list.txt: %v", err)
+	}
+
+	printf(stdout, "Original Count: %d\n", len(originalList))
+	printf(stdout, "To Filter Count: %d\n", len(filterList))
+	printf(stdout, "Final Count: %d\n", len(finalList))
+
+	return nil
+}
+
+// printf intentionally ignores write errors.
+func printf(w io.Writer, format string, args ...any) {
+	_, _ = fmt.Fprintf(w, format, args...)
+}
+
+// readLines reads a whole file into memory and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { err = errors.Join(err, f.Close()) }()
+
+	var lines []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, errors.Join(err, scanner.Err())
+}
+
+// writeLines writes the lines to the given file.
+func writeLines(path string, lines []string) (err error) {
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer func() { err = errors.Join(err, f.Close()) }()
+
+	w := bufio.NewWriter(f)
+	for _, line := range lines {
+		if _, werr := fmt.Fprintln(w, line); werr != nil {
+			return werr
 		}
 	}
-	fmt.Println("Original Count: ", originalCount)
-	fmt.Println("To Filter Count: ", toFilterCount)
-	fmt.Println("Final Count: ", finalCount)
+
+	return errors.Join(err, w.Flush())
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/cenkalti/backoff/v4 v4.3.0
 	github.com/google/uuid v1.6.0
 	github.com/jaswdr/faker/v2 v2.8.0
+	github.com/rogpeppe/go-internal v1.14.1
 	github.com/stephenafamo/bob v0.41.1
 	go.temporal.io/api v1.43.2
 	go.temporal.io/sdk v1.32.1
@@ -67,11 +68,12 @@ require (
 	go4.org v0.0.0-20230225012048-214862532bf5 // indirect
 	golang.org/x/exp v0.0.0-20250620022241-b7579e27df2b // indirect
 	golang.org/x/mod v0.26.0 // indirect
-	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/net v0.41.0 // indirect
 	golang.org/x/sync v0.15.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
-	golang.org/x/text v0.24.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
 	golang.org/x/time v0.11.0 // indirect
+	golang.org/x/tools v0.34.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250324211829-b45e905df463 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
 	google.golang.org/grpc v1.71.0 // indirect
